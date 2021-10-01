ef1999fd02cac64b03a8722bb92d7350db21ee5a
Nastya Luk <info@zrobimsvet.by>
2021-10-01 23:10:13 +0300

    my homework_3

---
+++ my_homework/DZ_3.py
@@ -0,0 +1,61 @@
+#only Tasks 3 - 7
+
+
+# Task 7
+
+
+# a = 10
+# years = 3
+# bank = a*1.1**years
+# print(bank)
+
+
+#Task 6
+
+
+# a = int(input())
+# b = int(input())
+ 
+# while a!=0 and b!=0:
+#     if a > b:
+#         a = a - b
+#     else:
+#         b = b - a
+ 
+# print (a+b)
+
+
+
+#Task 5
+
+# a = int(input())
+# sum = 0
+# while a > 0:
+#     number = a % 10
+#     sum = sum + number
+#     a = a // 10
+# print(sum)
+
+# Task 4
+
+# name = str(input('name:\n'))
+# if name == 'rectangle':
+#      length = int(input('length:\n')) 
+#      width = int(input('with:\n'))
+#      s = length * width
+# if name == 'triangle': 
+#     side_length_1 = int(input('e_length_1:\n'))
+#     side_length_2 = int(input('e_length_2:\n'))
+#     side_length_3 = int(input('e_length_3:\n'))
+#     p = (side_length_1 + side_length_2 + side_length_3) / 2
+#     s = (p * (p-side_length_1) * (p-side_length_2) * (p-side_length_3)) ** 0.5
+# if name == 'circle':
+#     radius = int(input('radius:\n'))
+#     s = 3.14 * radius **2
+# print(s)
+
+# Task 3
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# number = [x for x in a if x < 5]
+# print(number)
